afd2f1c536b991b3b161a169ccbb375ee1bfc6ae
kavyapotnuru258 <kavyapotnuru258@gmail.com>
2024-08-26 22:37:08 +0530

    Update typewise-alert.cpp

--- typewise-alert.cpp
+++ typewise-alert.cpp
@@ -1,11 +1,82 @@
+// #include "typewise-alert.h"
+// #include <stdio.h>
+
+// BreachType inferBreach(double value, double lowerLimit, double upperLimit) {
+//   if(value < lowerLimit) {
+//     return TOO_LOW;
+//   }
+//   if(value > upperLimit) {
+//     return TOO_HIGH;
+//   }
+//   return NORMAL;
+// }
+
+// BreachType classifyTemperatureBreach(
+//     CoolingType coolingType, double temperatureInC) {
+//   int lowerLimit = 0;
+//   int upperLimit = 0;
+//   switch(coolingType) {
+//     case PASSIVE_COOLING:
+//       lowerLimit = 0;
+//       upperLimit = 35;
+//       break;
+//     case HI_ACTIVE_COOLING:
+//       lowerLimit = 0;
+//       upperLimit = 45;
+//       break;
+//     case MED_ACTIVE_COOLING:
+//       lowerLimit = 0;
+//       upperLimit = 40;
+//       break;
+//   }
+//   return inferBreach(temperatureInC, lowerLimit, upperLimit);
+// }
+
+// void checkAndAlert(
+//     AlertTarget alertTarget, BatteryCharacter batteryChar, double temperatureInC) {
+
+//   BreachType breachType = classifyTemperatureBreach(
+//     batteryChar.coolingType, temperatureInC
+//   );
+
+//   switch(alertTarget) {
+//     case TO_CONTROLLER:
+//       sendToController(breachType);
+//       break;
+//     case TO_EMAIL:
+//       sendToEmail(breachType);
+//       break;
+//   }
+// }
+
+// void sendToController(BreachType breachType) {
+//   const unsigned short header = 0xfeed;
+//   printf("%x : %x\n", header, breachType);
+// }
+
+// void sendToEmail(BreachType breachType) {
+//   const char* recepient = "a.b@c.com";
+//   switch(breachType) {
+//     case TOO_LOW:
+//       printf("To: %s\n", recepient);
+//       printf("Hi, the temperature is too low\n");
+//       break;
+//     case TOO_HIGH:
+//       printf("To: %s\n", recepient);
+//       printf("Hi, the temperature is too high\n");
+//       break;
+//     case NORMAL:
+//       break;
+//   }
+// }
 #include "typewise-alert.h"
 #include <stdio.h>
 
 BreachType inferBreach(double value, double lowerLimit, double upperLimit) {
-  if(value < lowerLimit) {
+  if (value < lowerLimit) {
     return TOO_LOW;
   }
-  if(value > upperLimit) {
+  if (value > upperLimit) {
     return TOO_HIGH;
   }
   return NORMAL;
@@ -13,33 +84,21 @@ BreachType inferBreach(double value, double lowerLimit, double upperLimit) {
 
 BreachType classifyTemperatureBreach(
     CoolingType coolingType, double temperatureInC) {
-  int lowerLimit = 0;
-  int upperLimit = 0;
-  switch(coolingType) {
-    case PASSIVE_COOLING:
-      lowerLimit = 0;
-      upperLimit = 35;
-      break;
-    case HI_ACTIVE_COOLING:
-      lowerLimit = 0;
-      upperLimit = 45;
-      break;
-    case MED_ACTIVE_COOLING:
-      lowerLimit = 0;
-      upperLimit = 40;
-      break;
-  }
-  return inferBreach(temperatureInC, lowerLimit, upperLimit);
+  // Simplify with arrays for limits
+  const double lowerLimits[] = {0, 0, 0};
+  const double upperLimits[] = {35, 45, 40};
+  
+  return inferBreach(temperatureInC, lowerLimits[coolingType], upperLimits[coolingType]);
 }
 
 void checkAndAlert(
     AlertTarget alertTarget, BatteryCharacter batteryChar, double temperatureInC) {
-
+  
   BreachType breachType = classifyTemperatureBreach(
     batteryChar.coolingType, temperatureInC
   );
 
-  switch(alertTarget) {
+  switch (alertTarget) {
     case TO_CONTROLLER:
       sendToController(breachType);
       break;
@@ -55,17 +114,19 @@ void sendToController(BreachType breachType) {
 }
 
 void sendToEmail(BreachType breachType) {
-  const char* recepient = "a.b@c.com";
-  switch(breachType) {
+  const char* recipient = "a.b@c.com";
+  switch (breachType) {
     case TOO_LOW:
-      printf("To: %s\n", recepient);
+      printf("To: %s\n", recipient);
       printf("Hi, the temperature is too low\n");
       break;
     case TOO_HIGH:
-      printf("To: %s\n", recepient);
+      printf("To: %s\n", recipient);
       printf("Hi, the temperature is too high\n");
       break;
     case NORMAL:
+      // No email needed for NORMAL
       break;
   }
 }
+
