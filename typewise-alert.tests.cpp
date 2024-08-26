b4c45509c14ec2bb8fda4f96c35053cdeefd82ca
kavyapotnuru258 <kavyapotnuru258@gmail.com>
2024-08-26 22:37:37 +0530

    Update typewise-alert.tests.cpp

--- typewise-alert.tests.cpp
+++ typewise-alert.tests.cpp
@@ -1,7 +1,40 @@
+// #include <gtest/gtest.h>
+// #include "typewise-alert.h"
+
+// TEST(TypeWiseAlertTestSuite,InfersBreachAccordingToLimits) {
+
+
+// }
 #include <gtest/gtest.h>
 #include "typewise-alert.h"
 
-TEST(TypeWiseAlertTestSuite,InfersBreachAccordingToLimits) {
-
-
+TEST(TypeWiseAlertTestSuite, InfersBreachAccordingToLimits) {
+    // Test cases for inferBreach
+    EXPECT_EQ(inferBreach(25, 20, 30), NORMAL);
+    EXPECT_EQ(inferBreach(15, 20, 30), TOO_LOW);
+    EXPECT_EQ(inferBreach(35, 20, 30), TOO_HIGH);
 }
+
+TEST(TypeWiseAlertTestSuite, ClassifiesTemperatureBreach) {
+    // Test cases for classifyTemperatureBreach
+    EXPECT_EQ(classifyTemperatureBreach(PASSIVE_COOLING, 10), NORMAL);
+    EXPECT_EQ(classifyTemperatureBreach(PASSIVE_COOLING, 36), TOO_HIGH);
+    EXPECT_EQ(classifyTemperatureBreach(HI_ACTIVE_COOLING, 50), TOO_HIGH);
+    EXPECT_EQ(classifyTemperatureBreach(MED_ACTIVE_COOLING, 39), TOO_HIGH);
+    EXPECT_EQ(classifyTemperatureBreach(MED_ACTIVE_COOLING, 25), NORMAL);
+}
+
+TEST(TypeWiseAlertTestSuite, CheckAndAlert) {
+    // Test cases for checkAndAlert
+    testing::internal::CaptureStdout();
+    BatteryCharacter battery = {PASSIVE_COOLING, "BrandX"};
+    checkAndAlert(TO_CONTROLLER, battery, 30);
+    std::string output = testing::internal::GetCapturedStdout();
+    EXPECT_NE(output.find("feed"), std::string::npos);  // Check if header is printed
+
+    testing::internal::CaptureStdout();
+    checkAndAlert(TO_EMAIL, battery, 36);
+    output = testing::internal::GetCapturedStdout();
+    EXPECT_NE(output.find("Hi, the temperature is too high"), std::string::npos);  // Check alert message
+}
+
